b7a1e4adba6475423eca29f9abfc1d913075f3cc
kenfelix <kmrapper.kf@gmail.com>
2022-04-05 20:17:34 +0000

    new

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr = [];
   for (let idx of array) {
-    ar.push(appendString + val);
+    arr.push(appendString + val);
   }
 
   return arr;
