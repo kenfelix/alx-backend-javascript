3760282e7558bd8d98aedc3a20cdfd9537700edc
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-04-05 22:32:02 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,5 @@
 export default function createIteratorObject(report) {
-  const arrays = Object.values(report)
-  const ar = []
-  for (const arr of arrays) {
-    return ar.push(...arr)
+  for (let x of report) {
+    return Object.values(x);
   }
 }
