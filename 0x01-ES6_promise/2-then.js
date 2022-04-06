432df1e07265c5e7469d953c57ca83ce336f29e9
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-04-06 21:41:29 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,13 @@
 function handleResponseFromAPI(promise) {
+  new Promise((resolve, reject) => {
+    resolve({
+        status: 200,
+        body: 'Success'
+      });
+    reject(new Error(''));
+    }
+  });
+  
   promise.then(() => {
     console.log('Got a response from the API');
   }, (err) => err);
