98f55b6cf606a745726bc9040257ae742d3f0628
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-04-06 22:31:31 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,5 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
-    return {
-    status: 200,
-    body: 'photo-profile-1',
-    };
   }, (err) => err);
 }
