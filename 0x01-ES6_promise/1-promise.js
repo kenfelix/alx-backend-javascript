c48886b40cd5acb5ffcfea299ca755082f9e1e02
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-04-06 21:04:12 +0100

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 getFullResponseFromAPI(success) {
   const P = new Promise((resolve, reject) => {
     if (success === true) {
-      return {status: 200, body: 'Success'};
+      resolve({status: 200, body: 'Success'});
     } else {
-      return "The fake API is not working currently";
+      reject("The fake API is not working currently");
     }
   });
 }
